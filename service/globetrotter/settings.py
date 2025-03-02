8cd1f64ce49ca194b257eae652072a6d88c3cf61
aargeee <211220042@nitdelhi.ac.in>
2025-03-02 18:05:33 +0530

    add django secret in .env

--- service/globetrotter/settings.py
+++ service/globetrotter/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-btppfouy@!o-ef)o=i!n=9o-q4c$4@=a(d%n29x*hts0l8=ij%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
