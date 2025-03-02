fb50aad40e85a8933ba03bc7a4af1b70660dd465
aargeee <211220042@nitdelhi.ac.in>
2025-03-02 18:26:30 +0530

    admin page changes

--- service/game/admin.py
+++ service/game/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Destination, DestinationInfo, Game, Question
 
-admin.site.register(Destination)
+class DestinationInfoInline(admin.TabularInline):
+    model = DestinationInfo
+    extra = 1
+
+class DestinationAdmin(admin.ModelAdmin):
+    inlines = [DestinationInfoInline]
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+admin.site.register(Destination, DestinationAdmin)
 admin.site.register(DestinationInfo)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(Question)
